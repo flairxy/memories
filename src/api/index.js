d674e35f9eeb4f1ad5b197082bf7b14cd2978020
flairxy <okonkwo.miracle@raadaa.com>
2021-07-05 18:47:54 +0100

    change url

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000" });
+const API = axios.create({ baseURL: "https://flair-memories.herokuapp.com/" });
 
 API.interceptors.request.use((request) => {
   const data = localStorage.getItem("profile");
